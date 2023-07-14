0f84a40a1dbf90c7aff11b40744819941c9a6bc4
Jose Espinoza <espinozajgx@gmail.com>
2023-07-14 14:29:24 +0200

    chore: added classes

---
+++ src/main/java/com/espinozajgx/actions/controller/ApiController.java
@@ -0,0 +1,19 @@
+package com.espinozajgx.actions.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ApiController {
+
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> healhcheck(){
+        return new ResponseEntity<>("I am alive!", HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/com/espinozajgx/actions/controller/ApiControllerTest.java
@@ -0,0 +1,37 @@
+package com.espinozajgx.actions.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("HealhCkeck response message 'I am alive' and http 200")
+    public void testHealhCheck() throws Exception {
+       MvcResult mvcResult = mockMvc.perform(get("/api/v1/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value("I am alive!"))
+                .andReturn();
+
+        assertEquals("application/json",
+                mvcResult.getResponse().getContentType());
+
+        assertEquals("I am alive!",
+                mvcResult.getResponse().getContentAsString());
+
+    }
+}
