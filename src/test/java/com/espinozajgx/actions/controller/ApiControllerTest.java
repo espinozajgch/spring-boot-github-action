2070e1e161cad08a22587ce102e9727edde7cc5a
Jose Espinoza <espinozajgx@gmail.com>
2023-09-05 16:20:37 +0200

    ci: testing pipeline

--- src/test/java/com/espinozajgx/actions/controller/ApiControllerTest.java
+++ src/test/java/com/espinozajgx/actions/controller/ApiControllerTest.java
@@ -44,7 +44,7 @@ class ApiControllerTest {
         System.out.println("El resultado del módulo entre " + a + " y " + b + " es: " + result + " y su division es:" + (a/b));
 
         String number = "1234567";
-        String name = "Jose Espinoza";
+        String name = "Jose G. Espinoza Ch.";
         System.out.println("Ultimo numero es: " + number.substring(number.length()-1));
 
     }
